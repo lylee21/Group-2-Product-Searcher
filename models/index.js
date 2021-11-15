5324a51c37d113313c66d45b00b76cf5c45de2b5
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2021-11-15 16:29:22 +0800

    test code updated

--- models/index.js
+++ models/index.js
@@ -1,10 +1,29 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const pretty = require("pretty");
 const userInput = null; // update
+const request = require("request");
 
-Product.findAll({
-    where:{
-        name:{
-            [Op.like]: userInput
-        }
-    },
-    order:[["name", "ASC"]]
-})
+// Product.findAll({
+//     where: {
+//         name: {
+//             [Op.like]: userInput,
+//         },
+//     },
+//     order: [["name", "ASC"]],
+// });
+
+request("https://shopee.sg/", (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+
+        $("._1syN0y").each((i, el) => {
+            const title = $(el).find("._1YAByT").text();
+
+            // Write Row To CSV
+            writeStream.write(`${title} \n`);
+        });
+
+        console.log("Scraping Done...");
+    }
+});
