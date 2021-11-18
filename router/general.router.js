7ac86b1258d14d79a3e6a243fcfd7c1476f86139
Clemens <operations.sg65@gmail.com>
2021-11-18 12:22:11 +0700

    Update

--- router/general.router.js
+++
@@ -1,5 +0,0 @@
-const cheerio = require('cheerio');
-const express = require('express');
-const axios = require('axios');
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:');
